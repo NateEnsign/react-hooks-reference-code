dc5aa549bf13534c02951d95aad5257173de756e
Nate Ensign <dixienate@gmail.com>
2022-12-05 16:33:59 -0700

    router and nav code added in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
 import Header from './components/Header'
-import StateComponent from './components/StateComponent';
-import EffectComponent from './components/EffectComponent';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import DetailScreen from './screens/DetailScreen'
+import ReferenceScreen from './screens/ReferenceScreen';
 
 function App() {
   return (
     <div className="App col-container">
       <Header />
-      <StateComponent />
-      <EffectComponent />
+      <Routes>
+        <Route path="/" element={<HomeScreen />}></Route>
+        <Route path="/Reference" element={<ReferenceScreen />}></Route>
+        <Route path="/Details/:id" element={<DetailScreen />}></Route>
+      </Routes>
+    
     </div>
   );
 }
